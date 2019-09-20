1e341e77a0ec4b02e18fe4a385e8a94f032cd89c
falguierm <maxime.falguier@fabernovel.com>
2019-09-20 15:36:44 +0200

    optim display card

--- src/templates/list/listItem.tsx
+++ src/templates/list/listItem.tsx
@@ -11,36 +11,38 @@ type Props = {
 
 const ListItem: React.FC<Props> = ({data}) => {
   return (
-    <div className={styles.container}>
-      <img className={styles.fit} src={`${process.env.PUBLIC_URL}/${data.image_url}`} alt={data.name} />
-      <div className={styles.bottomDescription}>
-        <h1 className={styles.title}>{data.name}</h1>
-        <p className={styles.description}>{data.description}</p>
-        <div className={styles.row}>
-          <div>
-            {(data.location) &&
-              <div className={styles.address}>
-                <LocationIcon />
-                <div>
-                  <p className={styles.locationText}>{data.location.number} {data.location.address}</p>
-                  <p className={styles.locationText}>{data.location.postal_code}</p>
+    <div className={styles.item}>
+      <div className={styles.container}>
+        <img className={styles.fit} src={`${process.env.PUBLIC_URL}/${data.image_url}`} alt={data.name} />
+        <div className={styles.bottomDescription}>
+          <h1 className={styles.title}>{data.name}</h1>
+          <p className={styles.description}>{data.description}</p>
+          <div className={styles.row}>
+            <div>
+              {(data.location) &&
+                <div className={styles.address}>
+                  <LocationIcon />
+                  <div>
+                    <p className={styles.locationText}>{data.location.number} {data.location.address}</p>
+                    <p className={styles.locationText}>{data.location.postal_code}</p>
+                  </div>
                 </div>
-              </div>
-            }
-          </div>
-          <div>
-            <span className={styles.locationText}><EmployesIcon /> {data.employees_count}</span>
-            <span style={{marginLeft: "5px"}} className={styles.locationText}>
-            {(data.location) &&
-              <>
-              <LanguageIcon />
-              {data.location.language.map((lang, index, tab) =>
-              `${lang} ${(index+1 < tab.length) ? ' / ' : ''}`
-              )
               }
-              </>
-            }
-            </span>
+            </div>
+            <div>
+              <span className={styles.locationText}><EmployesIcon /> {data.employees_count}</span>
+              <span style={{marginLeft: "5px"}} className={styles.locationText}>
+              {(data.location) &&
+                <>
+                <LanguageIcon />
+                {data.location.language.map((lang, index, tab) =>
+                `${lang} ${(index+1 < tab.length) ? ' / ' : ''}`
+                )
+                }
+                </>
+              }
+              </span>
+            </div>
           </div>
         </div>
       </div>
