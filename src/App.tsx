13038d827e4eff0a404b4e436f65261dc1e059f1
Victor Chabbert <50482864+victorchabbert-ft@users.noreply.github.com>
2019-09-23 16:06:35 +0200

    Remove code for exercise 1: add filter to list

--- src/App.tsx
+++ src/App.tsx
@@ -3,71 +3,30 @@ import "./styles/global.scss";
 import styles from "./app.module.scss";
 import Header from "./templates/header";
 import OfficeList from "./templates/list/list";
-import { OfficeSearch, Continent, Office } from "./models/offices";
+import { OfficeSearch } from "./models/offices";
 
 type Sort = "asc" | "desc";
 type AppState = {
   offices: OfficeSearch;
-  continentSelected: Continent;
   sort: Sort;
 };
 
 class App extends React.Component<{}, AppState> {
-  initialList: any;
   state = {
     offices: {
       count: 0,
       data: []
     },
-    continentSelected: {
-      key: "",
-      label: "World"
-    },
     sort: "asc"
   } as AppState;
-
-  getContinentList = (offices: OfficeSearch) => {
-    const continentList = offices.data.map((office: Office) => {
-      return office.location.continent;
-    });
-
-    return [
-      {
-        key: "*",
-        label: "World"
-      },
-      ...continentList.filter((continent, index) => {
-        return (
-          continentList.findIndex(
-            continentObject => continentObject.key === continent.key
-          ) === index
-        );
-      })
-    ];
-  };
 
   componentDidMount() {
     fetch("http://fake.fabernovel.com/api/offices")
       .then(r => r.json())
       .then(offices => {
-        this.initialList = offices;
         this.setState({ offices });
       });
   }
-
-  onChangeContinent = (continent: Continent) => {
-    const officesFiltered = this.initialList.data.filter((office: Office) => {
-      return office.location.continent.key === continent.key;
-    });
-
-    this.setState({
-      continentSelected: continent,
-      offices: {
-        count: officesFiltered.length,
-        data: continent.key !== "*" ? officesFiltered : this.initialList.data
-      }
-    });
-  };
 
   handleSearch = (query: string) => {
     const queryString = query.length > 0 ? `?query=${query}` : "";
@@ -93,9 +52,6 @@ class App extends React.Component<{}, AppState> {
           sort={this.state.sort}
           onSortToggle={this.handleSort}
           onSearchSubmit={this.handleSearch}
-          continentSelected={this.state.continentSelected}
-          onChangeContinent={this.onChangeContinent}
-          continentList={this.getContinentList(this.initialList)}
         />
         <OfficeList offices={this.state.offices} sort={this.state.sort} />
       </div>
--- src/templates/header/header.tsx
+++ src/templates/header/header.tsx
@@ -2,38 +2,21 @@ import React from "react";
 import styles from "./header.module.css";
 import Logo from "../logo";
 import SearchInput from "../search-input";
-import Dropdown from "../dropdown/dropdown";
-import { Continent } from "../../models/offices";
 import Sort from "../sort";
 
 export type props = {
-  continentList: Continent[];
-  continentSelected: Continent;
-  onChangeContinent: (continent: Continent) => void;
   onSearchSubmit: (query: string) => void;
   sort: "asc" | "desc";
   onSortToggle: () => void;
 };
 
-const Header: React.FC<props> = ({
-  continentList,
-  continentSelected,
-  onChangeContinent,
-  onSearchSubmit,
-  sort,
-  onSortToggle
-}) => {
+const Header: React.FC<props> = ({ onSearchSubmit, sort, onSortToggle }) => {
   return (
     <div className={styles.header}>
       <Logo />
       <div className={styles.headerSearchbar}>
         <SearchInput onSubmit={onSearchSubmit} />
       </div>
-      <Dropdown
-        continentSelected={continentSelected}
-        onChangeContinent={onChangeContinent}
-        continentList={continentList}
-      />
       <Sort direction={sort} onClick={onSortToggle} />
     </div>
   );
