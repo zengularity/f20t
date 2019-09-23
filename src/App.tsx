b3fa670f80088c0a5ff47e84d1340f521c20b410
Victor Chabbert <50482864+victorchabbert-ft@users.noreply.github.com>
2019-09-23 16:40:38 +0200

    Remove listing container

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./styles/global.scss";
 import styles from "./app.module.scss";
 import Header from "./templates/header";
-import OfficeList from "./templates/list/list";
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <div className={styles.app}>
         <Header />
-        <OfficeList />
       </div>
     );
   }
